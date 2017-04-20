4c280a80a289ca29fc72386ecece05e5a88e3f32
CakeLancelot <soundwaver7@gmail.com>
2017-04-20 15:53:21 -0500

    Initial commit

---
+++ src/main/java/co/cakeface/studio/studio/Main.java
@@ -0,0 +1,17 @@
+package co.cakeface.studio.studio;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("hello");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
